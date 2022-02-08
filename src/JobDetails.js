167845038f21707cd68479fb6cf7d2b3193deecf
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-02-08 15:11:02 +0100

    src/JobDetails: add passed/failed jobs counts to the worker table in stats

--- src/JobDetails.js
+++ src/JobDetails.js
@@ -395,6 +395,8 @@ const Worker = (props) => {
             <td>{props.worker.runtime_min.toFixed(2)}</td>
             <td>{props.worker.runtime_max.toFixed(2)}</td>
             <td>{props.worker.total_cpu_time.toFixed(2)}</td>
+            <td>{props.worker.jobs_passed}</td>
+            <td>{props.worker.jobs_failed}</td>
             <td>{props.worker.jobs_count}</td>
         </tr>
     );
@@ -419,7 +421,7 @@ const JobStats = (props) => {
             <div className="card m-1">
                 <div className="card-header">
                 <div className="row align-items-center">
-                    <div className="col-md-8">Workers stats</div>
+                    <div className="col-md-8">Workers ({props.stats.workers.filter(worker => worker.name.includes(filter)).length})</div>
                     <div className="col-md-4">
                         <input className="form-control pull-right" type="text" placeholder="Filter workers" onChange={(event) => {setFilter(event.target.value)}} />
                     </div>
@@ -429,11 +431,13 @@ const JobStats = (props) => {
                     <table className="table">
                         <thead>
                             <tr>
-                                <th scope="col">Worker</th>
+                                <th scope="col">Name</th>
                                 <th scope="col">Average time (s)</th>
                                 <th scope="col">Min time (s)</th>
                                 <th scope="col">Max time (s)</th>
                                 <th scope="col">Total CPU time (s)</th>
+                                <th scope="col">Passed jobs</th>
+                                <th scope="col">Failed jobs</th>
                                 <th scope="col">Total jobs</th>
                             </tr>
                         </thead>
