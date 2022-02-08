7bb0da03afada712ef322956e8a8c72a283ae9e2
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-02-08 10:36:53 +0100

    JobDetails: add min/max time in workers stats

--- src/JobDetails.js
+++ src/JobDetails.js
@@ -392,6 +392,8 @@ const Worker = (props) => {
         <tr>
             <th scope="row">{props.worker.name}</th>
             <td>{props.worker.runtime_avg.toFixed(2)}</td>
+            <td>{props.worker.runtime_min.toFixed(2)}</td>
+            <td>{props.worker.runtime_max.toFixed(2)}</td>
             <td>{props.worker.total_cpu_time.toFixed(2)}</td>
             <td>{props.worker.jobs_count}</td>
         </tr>
@@ -407,9 +409,9 @@ const JobStats = (props) => {
                 <div className="card-header">Global stats</div>
                 <div className="card-body">
                     <ul className="list-group">
-                        <li className="list-group-item">Total jobs: {props.stats.total_jobs}</li>
-                        <li className="list-group-item">Total builds: {props.stats.total_builds}</li>
+                        {(props.stats.total_builds > 0) && <li className="list-group-item">Total builds: {props.stats.total_builds}</li>}
                         {(props.stats.total_tests > 0) && <li className="list-group-item">Total tests: {props.stats.total_tests}</li>}
+                        {(props.stats.total_builds > 0) && (props.stats.total_tests > 0) && <li className="list-group-item">Total jobs: {props.stats.total_jobs}</li>}
                         <li className="list-group-item">Total CPU time: {props.stats.total_time}</li>
                     </ul>
                 </div>
@@ -429,6 +431,8 @@ const JobStats = (props) => {
                             <tr>
                                 <th scope="col">Worker</th>
                                 <th scope="col">Average time (s)</th>
+                                <th scope="col">Min time (s)</th>
+                                <th scope="col">Max time (s)</th>
                                 <th scope="col">Total CPU time (s)</th>
                                 <th scope="col">Total jobs</th>
                             </tr>
